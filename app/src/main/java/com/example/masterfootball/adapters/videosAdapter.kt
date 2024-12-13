0bb6cced77ac7f38d8169b4aff0283766ba6da0e
Manel <manel.delahoz@mataro.epiaedu.cat>
2024-12-13 17:19:39 +0100

    new

---
+++ app/src/main/java/com/example/masterfootball/adapters/videosAdapter.kt
@@ -0,0 +1,55 @@
+package com.example.masterfootball.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.masterfootball.R
+import com.example.masterfootball.classes.Video
+
+class videosAdapter(private var onCardClicked: ((video: Video) -> Unit)): RecyclerView.Adapter<videosAdapter.ViewHolder>() {
+    var videos: MutableList<Video> = ArrayList()
+    lateinit var context: Context
+
+    fun videosAdapter(alumnos: MutableList<Video>, context: Context) {
+        this.videos = alumnos
+        this.context = context
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = videos.get(position)
+        holder.bind(item, context)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        return ViewHolder(layoutInflater.inflate(R.layout.card, parent, false))
+    }
+
+    override fun getItemCount(): Int {
+        return videos.size
+    }
+
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val name = view.findViewById<TextView>(R.id.textView5)
+        val unlockedStatus = view.findViewById<ImageView>(R.id.imageView4)
+        val card = view.findViewById<CardView>(R.id.card)
+
+        fun bind(video:Video, context: Context) {
+            name.text = video.name
+            if (!video.unlocked) {
+                unlockedStatus.setImageResource(R.drawable.sintitulo)
+            }
+            else {
+                unlockedStatus.setImageResource(0)
+            }
+            card.setOnClickListener {
+                onCardClicked(video)
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/masterfootball/menuPrincipal.kt
@@ -0,0 +1,43 @@
+package com.example.masterfootball
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.content.Intent
+import android.net.Uri
+import androidx.activity.enableEdgeToEdge
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.masterfootball.databinding.ActivityMainBinding
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
+import kotlin.random.Random
+import android.view.View
+import java.security.MessageDigest
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.sql.Connection
+import java.sql.DriverManager
+import java.sql.ResultSet
+import com.example.masterfootball.classes.bdConnection
+import com.example.masterfootball.classes.Users
+import com.google.android.material.snackbar.Snackbar
+
+class menuPrincipal: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.main_menu)
+    }
+
+    fun videosClick(view: View) {
+        val i = Intent(this, Videos::class.java)
+        startActivity(i)
+    }
+}
