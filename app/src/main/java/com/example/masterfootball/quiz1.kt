7c858e45766ee153f11d289ab5edc051bee90369
Manel <manel.delahoz@mataro.epiaedu.cat>
2025-01-15 20:06:30 +0100

    quiz1 created finished

--- app/src/main/java/com/example/masterfootball/quiz1.kt
+++ app/src/main/java/com/example/masterfootball/quiz1.kt
@@ -21,21 +21,39 @@ import com.example.masterfootball.adapters.videosAdapter
 import com.example.masterfootball.classes.QuestionQuiz
 import com.example.masterfootball.classes.Questions
 import com.example.masterfootball.classes.Video
+import com.example.masterfootball.classes.bdConnection
 import com.google.android.material.snackbar.Snackbar
 import com.google.gson.Gson
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.sql.Connection
+import java.sql.DriverManager
+import java.time.LocalDate
+import java.time.LocalTime
+import java.time.LocalDateTime
+import java.sql.Date
+import java.sql.Time
+import java.time.ZoneOffset
 
 class quiz1 : AppCompatActivity() {
     lateinit var myRecyclerView : RecyclerView
     var questionsList : MutableList<QuestionQuiz> = ArrayList()
     var c = 0
     var number = 0
+    var id: Int = 0
     lateinit var question: TextView
     lateinit var option1: TextView
     lateinit var option2: TextView
     lateinit var option3: TextView
     lateinit var option4: TextView
+    lateinit var notaText: TextView
     lateinit var questionsLayout: ConstraintLayout
     lateinit var reviewLayout: ConstraintLayout
+    lateinit var noteLayout: ConstraintLayout
+    var nota: Int = 0
+    lateinit var data: Date
+    lateinit var hora: Time
     val mAdapter : quizAnswersAdapter = quizAnswersAdapter()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -47,6 +65,7 @@ class quiz1 : AppCompatActivity() {
             insets
         }
         number = intent.extras!!.getInt("number")
+        id = intent.extras!!.getInt("idUser")
         question = findViewById(R.id.question)
         option1 = findViewById(R.id.opcio1)
         option2 = findViewById(R.id.opcio2)
@@ -54,6 +73,8 @@ class quiz1 : AppCompatActivity() {
         option4 = findViewById(R.id.opcio4)
         questionsLayout = findViewById(R.id.questionsLayaout)
         reviewLayout = findViewById(R.id.reviewLayaout)
+        noteLayout = findViewById(R.id.finishLayaout)
+        notaText = findViewById(R.id.cualificacion)
         loadSimple()
         quizCreate()
     }
@@ -138,5 +159,58 @@ class quiz1 : AppCompatActivity() {
         mAdapter.quizAnswersAdapter(questionsList, this)
 
         myRecyclerView.adapter = mAdapter
+    }
+
+    fun acabarQuiz(view: View) {
+        reviewLayout.visibility = View.GONE
+        for (question in questionsList) {
+            if (question.selectedOption.equals(question.correctOption)) {
+                nota++
+            }
+        }
+        guardaIntent(number)
+        noteLayout.visibility = View.VISIBLE
+        notaText.setText(notaText.text.toString() + " " + nota + "/10")
+    }
+
+    fun guardaIntent(number: Int) {
+        val fechaHoraActual: LocalDateTime = LocalDateTime.now()
+        val fecha: LocalDate = fechaHoraActual.toLocalDate()
+        data = Date(fecha.year - 1900, fecha.monthValue - 1, fecha.dayOfMonth)
+        val horaSQL: LocalTime = fechaHoraActual.toLocalTime()
+        hora = Time((horaSQL.hour * 3600 + horaSQL.minute * 60 + horaSQL.second) * 1000L)
+        CoroutineScope(Dispatchers.IO).launch {
+            val isQuizOpenSuccessful = guardaIntentConnection(number)
+            runOnUiThread {
+                if (isQuizOpenSuccessful != null) {
+                    Snackbar.make(findViewById<View>(android.R.id.content),"respuesta registrada correctamente", Snackbar.LENGTH_LONG)
+                        .show()
+                }
+            }
+        }
+    }
+    private fun guardaIntentConnection(numero: Int): Boolean {
+        var dbconfiguration: bdConnection = bdConnection()
+        var connection: Connection? = null
+        return try {
+            connection = DriverManager.getConnection(dbconfiguration.dbUrl, dbconfiguration.dbUser, dbconfiguration.dbPassword)
+
+            val query = "INSERT INTO quizintents VALUES (?, ?, ?, ?, ?, ?)"
+            val preparedStatement = connection.prepareStatement(query)
+            preparedStatement.setInt(1, 0)
+            preparedStatement.setInt(2, numero)
+            preparedStatement.setDate(3, data)
+            preparedStatement.setTime(4, hora)
+            preparedStatement.setInt(5, id)
+            preparedStatement.setInt(6, nota)
+
+            val resultSet = preparedStatement.executeUpdate()
+            resultSet > 0
+        } catch (e: Exception) {
+            e.printStackTrace()
+            false
+        } finally {
+            connection?.close()
+        }
     }
 }
