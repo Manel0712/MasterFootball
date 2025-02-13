6f2314ae7b11518fdc7d47d0c982ad7ba62dae98
Manel <manel.delahoz@mataro.epiaedu.cat>
2025-02-13 19:47:34 +0100

    Trivial

--- app/src/main/java/com/example/masterfootball/trivial.kt
+++ app/src/main/java/com/example/masterfootball/trivial.kt
@@ -2,35 +2,56 @@ package com.example.masterfootball
 
 import android.os.Bundle
 import android.view.Menu
+import android.view.View
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.masterfootball.adapters.trivialAdapter
 import com.example.masterfootball.classes.QuestionsTrivial
 import com.example.masterfootball.classes.preguntasTrivial
 import com.google.gson.Gson
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsControllerCompat
 
 class trivial : AppCompatActivity() {
 
+    lateinit var myRecyclerView : RecyclerView
+    val mAdapter : trivialAdapter = trivialAdapter()
     lateinit var pregunta: TextView
     lateinit var opcio1: TextView
     lateinit var opcio2: TextView
+    lateinit var questionsTrivialLayout: ConstraintLayout
+    lateinit var reviewLayout: ConstraintLayout
+    lateinit var noteLayout: ConstraintLayout
     lateinit var opcio3: TextView
     lateinit var numPregunta: TextView
-
+    private var c: Int = 0
     private var currentIndex = 0
-    private var questionsList = mutableListOf<QuestionsTrivial>()
+    private var questionsList: MutableList<QuestionsTrivial> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.trivial)
         supportActionBar?.setDisplayShowTitleEnabled(false)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        window.statusBarColor = getColor(R.color.black)
+        WindowInsetsControllerCompat(window, window.decorView).isAppearanceLightStatusBars = false
 
+        // Referencia a los TextView
         pregunta = findViewById(R.id.tvPreguntatrivial)
         opcio1 = findViewById(R.id.trivialOpcio1)
         opcio2 = findViewById(R.id.trivialOpcio2)
         opcio3 = findViewById(R.id.trivialOpcio3)
         numPregunta = findViewById(R.id.tvNumTrivial)
 
+        // Referencia a los layouts
+        questionsTrivialLayout = findViewById(R.id.questionsLayout)
+        reviewLayout = findViewById(R.id.reviewLayout)
+
+        // Cargar preguntas y mostrar la primera
         loadQuestions()
         showQuestion()
 
@@ -47,10 +68,12 @@ class trivial : AppCompatActivity() {
 
     private fun loadQuestions() {
         val jsonFile = "trivial.json"
-        val json: String? = this.assets.open(jsonFile).bufferedReader().use { it.readText() }
+        val json: String = this.assets.open(jsonFile).bufferedReader().use { it.readText() }
         val gson = Gson()
-        val newTrivial: preguntasTrivial = gson.fromJson(json, preguntasTrivial::class.java)
-        questionsList = newTrivial.questions.toMutableList()
+        var questionsTrivial: preguntasTrivial = gson.fromJson(json, preguntasTrivial::class.java)
+        questionsTrivial.questions.forEach {
+            questionsList.add(QuestionsTrivial(it.question, it.option1, it.option2, it.option3, it.answer))
+        }
     }
 
     private fun showQuestion() {
@@ -89,19 +112,37 @@ class trivial : AppCompatActivity() {
             else -> -1
         }
 
+        if (selectedOption==1) {
+            questionsList[currentIndex].selectedOption = questionsList[currentIndex].option1
+        }
+        else if (selectedOption==2) {
+            questionsList[currentIndex].selectedOption = questionsList[currentIndex].option2
+        }
+        else {
+            questionsList[currentIndex].selectedOption = questionsList[currentIndex].option3
+        }
+
         if (selectedOption == correctOption) {
             // Respuesta correcta
-            setOptionColor(selectedOption, R.color.correcto)
+            setOptionColor(selectedOption, R.color.respuestaCorrecta)
         } else {
             // Respuesta incorrecta
-            setOptionColor(selectedOption, R.color.incorrecto)
-            setOptionColor(correctOption, R.color.correcto)
+            setOptionColor(selectedOption, R.color.respuestaIncorrecta)
+            setOptionColor(correctOption, R.color.respuestaCorrecta)
         }
 
         // Mostrar la siguiente pregunta después de un breve retraso
         opcio1.postDelayed({
-            currentIndex++
-            showQuestion()
+            if (c<19) {
+                currentIndex++
+                c++
+                showQuestion()
+            }
+            else {
+                questionsTrivialLayout.visibility = View.GONE
+                reviewLayout.visibility = View.VISIBLE
+                setUpRecyclerView()
+            }
         }, 1500)
     }
 
@@ -112,5 +153,15 @@ class trivial : AppCompatActivity() {
             2 -> opcio2.setBackgroundColor(color)
             3 -> opcio3.setBackgroundColor(color)
         }
+    }
+
+    fun setUpRecyclerView(){
+        myRecyclerView = findViewById(R.id.yourAnswersList) as RecyclerView
+        myRecyclerView.setHasFixedSize(true)
+        myRecyclerView.layoutManager = LinearLayoutManager(this)
+
+        mAdapter.trivialAdapter(questionsList, this)
+
+        myRecyclerView.adapter = mAdapter
     }
 }
