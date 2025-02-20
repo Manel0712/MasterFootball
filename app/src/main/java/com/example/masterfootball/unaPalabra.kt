60942744cbddbfeabc5287635524ae3cc4d8aa66
Marionaaa22 <mariona.claros@mataro.epiaedu.cat>
2025-02-20 20:08:33 +0100

    Trivial completo, falta corregir y probar

--- app/src/main/java/com/example/masterfootball/unaPalabra.kt
+++ app/src/main/java/com/example/masterfootball/unaPalabra.kt
@@ -1,52 +1,59 @@
 package com.example.masterfootball
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
-import androidx.constraintlayout.widget.ConstraintLayout
-import com.example.masterfootball.classes.unaPalabra
+import android.widget.Toast
+import com.bumptech.glide.Glide
+import com.example.masterfootball.classes.PalabraJuego
+import com.example.masterfootball.databinding.UnaPalabraBinding
 import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import java.io.InputStreamReader
 
 class unaPalabra : AppCompatActivity() {
-    var questionsList: MutableList<unaPalabra> = ArrayList()
-    var currentQuestionIndex = 0
-    lateinit var img1: ImageView
-    lateinit var img2: ImageView
-    lateinit var img3: ImageView
-    lateinit var img4: ImageView
-    lateinit var inputResposta: EditText
-    lateinit var btnComprobar: Button
-    lateinit var gameLayout: ConstraintLayout
-    lateinit var resultLayout: ConstraintLayout
+    private var questionsList: MutableList<UnaPalabra> = mutableListOf()
+    private var currentQuestionIndex = 0
+
+    private lateinit var img1: ImageView
+    private lateinit var img2: ImageView
+    private lateinit var img3: ImageView
+    private lateinit var img4: ImageView
+    private lateinit var inputResposta: EditText
+    private lateinit var btnComprobar: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.una_palabra)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        setContentView(R.layout.una_palabra)  // Asegúrate de que este es el nombre correcto del layout XML
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(android.R.id.content)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
+        // Inicializar vistas
         img1 = findViewById(R.id.img1)
         img2 = findViewById(R.id.img2)
         img3 = findViewById(R.id.img3)
         img4 = findViewById(R.id.img4)
         inputResposta = findViewById(R.id.inputResposta)
         btnComprobar = findViewById(R.id.btnComprobar)
-        gameLayout = findViewById(R.id.gameLayout)
-        resultLayout = findViewById(R.id.resultLayout)
 
+        // Cargar preguntas y mostrar la primera
         loadQuestions()
-        loadQuestion(currentQuestionIndex)
+        if (questionsList.isNotEmpty()) {
+            loadQuestion(currentQuestionIndex)
+        } else {
+            Toast.makeText(this, "Error al cargar las preguntas", Toast.LENGTH_LONG).show()
+        }
 
         btnComprobar.setOnClickListener {
             checkAnswer()
@@ -54,19 +61,28 @@ class unaPalabra : AppCompatActivity() {
     }
 
     private fun loadQuestions() {
-        val jsonStream = assets.open("unaPalabra.json")
-        val jsonReader = InputStreamReader(jsonStream)
-        val gson = Gson()
-        val questionsData = gson.fromJson(jsonReader, Array<unaPalabra>::class.java).toList()
-        questionsList.addAll(questionsData)
+        try {
+            val jsonStream = assets.open("unaPalabra.json")
+            val jsonReader = InputStreamReader(jsonStream)
+            val gson = Gson()
+            val type = object : TypeToken<List<PalabraJuego>>() {}.type
+            questionsList = gson.fromJson(jsonReader, type) ?: mutableListOf()
+        } catch (e: Exception) {
+            e.printStackTrace()
+            Toast.makeText(this, "No se pudo cargar el archivo JSON", Toast.LENGTH_LONG).show()
+        }
     }
 
     private fun loadQuestion(index: Int) {
         val question = questionsList[index]
+
         Glide.with(this).load(question.images[0]).into(img1)
         Glide.with(this).load(question.images[1]).into(img2)
         Glide.with(this).load(question.images[2]).into(img3)
         Glide.with(this).load(question.images[3]).into(img4)
+
+        // Limpiar la respuesta anterior
+        inputResposta.text.clear()
     }
 
     private fun checkAnswer() {
@@ -77,10 +93,10 @@ class unaPalabra : AppCompatActivity() {
             currentQuestionIndex++
             if (currentQuestionIndex < questionsList.size) {
                 loadQuestion(currentQuestionIndex)
-                inputResposta.text.clear()
             } else {
-                gameLayout.visibility = View.GONE
-                resultLayout.visibility = View.VISIBLE
+                val intent = Intent(this, Juegos::class.java)
+                startActivity(intent)
+                finish()
             }
         } else {
             inputResposta.error = "Respuesta incorrecta. Inténtalo de nuevo."
